b6eb9fae69aa3f96868f8468fca42463d41e04ff
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-20 12:44:17 +0530

    basic frame created

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+        jFrame.setSize(360, 640);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setResizable(false);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }

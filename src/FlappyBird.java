ed2b39852223db52167f27e131e4f2109bd364c5
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-20 13:27:35 +0530

    images loaded

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,12 +1,33 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class FlappyBird extends JPanel {
     int boardWidth = 360;
     int boardHeight = 640;
 
+    //Images
+    Image backgroundImage;
+    Image flappyBirdImage;
+    Image topPipeImage;
+    Image bottomPipeImage;
     FlappyBird(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
-        setBackground(Color.BLUE);
+//        setBackground(Color.BLUE);
+
+        //load images
+        backgroundImage = new ImageIcon(Objects.requireNonNull(getClass().getResource("./assets/flappybirdbg.png"))).getImage();
+        flappyBirdImage = new ImageIcon(Objects.requireNonNull(getClass().getResource("assets/flappybird.png"))).getImage();
+        topPipeImage = new ImageIcon(Objects.requireNonNull(getClass().getResource("assets/toppipe.png"))).getImage();
+        bottomPipeImage = new ImageIcon(Objects.requireNonNull(getClass().getResource("assets/bottompipe.png"))).getImage();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    private void draw(Graphics g) {
+        g.drawImage(backgroundImage, 0, 0, boardWidth, boardHeight, null);
     }
 }

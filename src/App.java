4b0bca745d44635c05594e4d1d183ef78633795d
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-20 12:52:44 +0530

    new panel added to frame

--- src/App.java
+++ src/App.java
@@ -3,10 +3,15 @@ import javax.swing.*;
 public class App {
     public static void main(String[] args) throws Exception {
         JFrame jFrame = new JFrame();
-        jFrame.setVisible(true);
+//        jFrame.setVisible(true);
         jFrame.setSize(360, 640);
         jFrame.setLocationRelativeTo(null);
         jFrame.setResizable(false);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FlappyBird flappyBird = new FlappyBird();
+        jFrame.add(flappyBird);
+        jFrame.pack();
+        jFrame.setVisible(true);
     }
 }
